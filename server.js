9af736d5dd33b776386d6c82c2c4a0cc48db4eff
Tomasz Rosik <Bosik27@gmail.com>
2020-10-25 21:53:30 +0100

    Add form validation

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.engine('.hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout:
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -32,6 +34,19 @@ app.get('/history', (req,res) => {
 
 app.get('/hello/:name', (req, res) => {
   res.render('hello', {name: req.params.name });
+});
+
+app.post('/contact/send-message', (req, res) => {
+  
+  const {author, sender, title, message, fileName } = req.body;
+  
+
+  if(author && sender && title && message && fileName) {
+    res.render('contact', { isSent: true, fileValue: fileName });
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
 });
 
 app.use((req, res) => {
